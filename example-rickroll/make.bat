0c2cbca2c4e9cade84fc8e8dbc82a7b675b147f9
T0biasCZe <44525446+T0biasCZe@users.noreply.github.com>
2021-07-26 18:17:38 +0200

    Add files via upload

---
+++ example-rickroll/make.bat
@@ -0,0 +1,23 @@
+REM Automatically generated from Makefile
+..\..\bin\lcc -Wa-l -Wl-m  -c -o banks.o banks.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_1.o srcfile_1.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_2.o srcfile_2.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_3.o srcfile_3.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_4.o srcfile_4.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_5.o srcfile_5.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_6.o srcfile_6.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_7.o srcfile_7.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_8.o srcfile_8.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_9.o srcfile_9.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_10.o srcfile_10.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_11.o srcfile_11.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_12.o srcfile_12.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_13.o srcfile_13.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_14.o srcfile_14.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_15.o srcfile_15.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_16.o srcfile_16.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_17.o srcfile_17.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_18.o srcfile_18.c
+..\..\bin\lcc -Wa-l -Wl-m  -c -o srcfile_19.o srcfile_19.c
+..\..\bin\lcc -Wa-l -Wl-m  -autobank -Wb-ext=.rel -Wb-v -Wl-yt0x1B -Wl-yoA -Wl-ya4 -o demo.gb banks.o srcfile_1.o srcfile_2.o srcfile_3.o srcfile_4.o srcfile_5.o srcfile_6.o srcfile_7.o srcfile_8.o srcfile_9.o srcfile_10.o srcfile_11.o srcfile_12.o srcfile_13.o srcfile_14.o srcfile_15.o srcfile_16.o srcfile_17.o srcfile_18.o srcfile_19.o
+pause
